4fa95446ad56a535385b1d1f42d475ca2c87d935
Zekas-chan <philar-9@student.ltu.se>
2025-03-25 16:35:17 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module d0024e.exupg_bibliotekssystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens d0024e.exupg_bibliotekssystem to javafx.fxml;
+    exports d0024e.exupg_bibliotekssystem;
+}

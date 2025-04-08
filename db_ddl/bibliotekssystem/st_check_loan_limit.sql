1bd87f41a4dc5f2767df8c71dffaefe75ec794bc
Zekas-chan <philar-9@student.ltu.se>
2025-04-08 19:09:17 +0200

    Förbättrade check_loan_limit till att kolla om användaren överskrider lånegränsen innan inserts sker baserat på om nuvarande lån + nya lån > tillåtna lån, snarare än genom nuvarande lån > tillåtna lån.

---
+++ db_ddl/bibliotekssystem/st_check_loan_limit.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE FUNCTION bibliotekssystem.st_check_loan_limit() RETURNS trigger
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    current_loans INT := sf_getcurrentloanscount(new.användare_id);
+    loan_limit    INT := sf_getloanlimit(new.användare_id);
+    incoming_loans INT;
+BEGIN
+    SELECT COUNT(new.lån_id) INTO incoming_loans;
+    IF current_loans + incoming_loans > loan_limit THEN
+        RAISE EXCEPTION 'User has reached the maximum allowed simultaneous loans.';
+    END IF;
+    RETURN new;
+END;
+$$;
+

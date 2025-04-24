7a284869168585d56fb970a423a15a26594b1a78
Zekas-chan <philar-9@student.ltu.se>
2025-04-24 02:31:30 +0200

    En vy och en alternativ enklare sökfunktion. Återstår att se hur de går att använda bäst.

---
+++ db_ddl/bibliotekssystem/sf_sök_böcker_enkel.sql
@@ -0,0 +1,28 @@
+CREATE OR REPLACE FUNCTION bibliotekssystem.sf_sök_böcker_enkel("i_sökterm" text)
+    RETURNS TABLE(bok_id integer, isbn_13 character varying, titel character varying, "författare" text, "ämnesord" text)
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    RETURN QUERY
+        SELECT
+            b.bok_id,
+            b.isbn_13,
+            b.titel,
+            STRING_AGG(DISTINCT f.förnamn || ' ' || f.efternamn, ', ') AS författare,
+            STRING_AGG(DISTINCT a.ord, ', ') AS ämnesord
+        FROM bibliotekssystem."Bok" b
+                 LEFT JOIN bibliotekssystem."Bok_Författare" bf ON b.bok_id = bf.bok_id
+                 LEFT JOIN bibliotekssystem."Författare" f ON bf.författare_id = f.författare_id
+                 LEFT JOIN bibliotekssystem."Bok_Ämnesord" ba ON b.bok_id = ba.bok_id
+                 LEFT JOIN bibliotekssystem."Ämnesord" a ON ba.ord_id = a.ord_id
+        GROUP BY b.bok_id, b.isbn_13, b.titel
+        HAVING i_sökterm IS NULL OR (
+            b.titel ILIKE '%' || i_sökterm || '%' OR
+            b.isbn_13 ILIKE '%' || i_sökterm || '%' OR
+            STRING_AGG(DISTINCT f.förnamn || ' ' || f.efternamn, ', ') ILIKE '%' || i_sökterm || '%' OR
+            STRING_AGG(DISTINCT a.ord, ', ') ILIKE '%' || i_sökterm || '%'
+        );
+END;
+$$;
+
---
+++ db_ddl/bibliotekssystem/vy_bok_info.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE VIEW bibliotekssystem.view_bok_info(bok_id, titel, isbn_13, författare, ämnesord) AS
+SELECT b.bok_id,
+       b.titel,
+       b.isbn_13,
+       STRING_AGG(DISTINCT (f."förnamn"::TEXT || ' '::TEXT) || f.efternamn::TEXT, ', '::TEXT) AS "författare",
+       STRING_AGG(DISTINCT a.ord::TEXT, ', '::TEXT)                                           AS "ämnesord"
+FROM bibliotekssystem."Bok" b
+         LEFT JOIN bibliotekssystem."Bok_Författare" bf ON b.bok_id = bf.bok_id
+         LEFT JOIN bibliotekssystem."Författare" f ON bf."författare_id" = f."författare_id"
+         LEFT JOIN bibliotekssystem."Bok_Ämnesord" ba ON b.bok_id = ba.bok_id
+         LEFT JOIN bibliotekssystem."Ämnesord" a ON ba.ord_id = a.ord_id
+GROUP BY b.bok_id, b.titel, b.isbn_13;
+

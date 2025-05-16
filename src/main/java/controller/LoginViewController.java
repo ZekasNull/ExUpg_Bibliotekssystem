2216d6e7ef0ab2894fe93c1cb3e82c803e6c9bb1
Zekas-chan <philar-9@student.ltu.se>
2025-05-17 00:24:29 +0200

    Småfixat.
    Debugprints fick mer detalj och centraliserade kontrollen.
    
    Fixade att kod fortfarande körde när fel upptäckts i inmatning.
    
    Fixade också att den försökte göra set på användare när användaren inte lyckades logga in.
    
    Raderade showErrorPopup då den finns i superklassen nu.

--- src/main/java/controller/LoginViewController.java
+++ src/main/java/controller/LoginViewController.java
@@ -1,53 +1,53 @@
 package controller;
 
+import d0024e.exupg_bibliotekssystem.MainApplication;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import model.Användare;
 import state.ApplicationState;
 
 import java.util.Observable;
 
 public class LoginViewController extends Controller{
+    private final boolean DEBUGPRINTOUTS = MainApplication.DEBUGPRINTS;
     private ApplicationState state = ApplicationState.getInstance();
     @FXML
     public Button logInButton;
     public TextField idealBoxContents;
     public PasswordField pinBoxContents;
 
-    private void showErrorPopup(String message) {
-        Alert alert = new Alert(Alert.AlertType.ERROR);
-        alert.setTitle("Fel");
-        alert.setHeaderText(message);
-
-        alert.showAndWait();
-    }
-
     public void onLogInButtonCLick(ActionEvent actionEvent) {
         if (idealBoxContents.getText().isEmpty() && pinBoxContents.getText().isEmpty()) {
             showErrorPopup("Du måste ange ditt ideal och ditt pinnummer");
+            return;
         } else if (idealBoxContents.getText().isEmpty()) {
             showErrorPopup("Du måste ange ditt ideal");
+            return;
         } else if (pinBoxContents.getText().isEmpty()) {
             showErrorPopup("Du måste ange ditt pinnummer");
+            return;
         }
         //TODO kontrollern ska ansvara för att analysera informationen i användaren som hämtas (dvs rätt användartyp)
-        System.out.println("onloginbutton");
-        state.setCurrentUser(state.databaseService.logInUser(idealBoxContents.getText(), pinBoxContents.getText()));
+        if (DEBUGPRINTOUTS) System.out.println("LoginViewController: onloginbutton");
+        Användare user = state.databaseService.logInUser(idealBoxContents.getText(), pinBoxContents.getText());
+        if (user != null) state.setCurrentUser(user);
     }
 
     @Override
     public void update(Observable o, Object arg) {
-        System.out.println("State changed, updating loginview");
+        if(arg != ApplicationState.UpdateType.USER) return;
+        if (DEBUGPRINTOUTS) System.out.println("LoginViewController: State changed");
         boolean success = state.getCurrentUser() != null;
         Alert alert = new Alert(success ? Alert.AlertType.INFORMATION : Alert.AlertType.ERROR);
         alert.setTitle("Information");
         alert.setHeaderText("Login attempt");
         alert.setContentText("The login attempt was" + (success ? "" : "n't") + " successful.");
 
-        System.out.println(state.getCurrentUser());//Sanitycheck
+        if (DEBUGPRINTOUTS) System.out.println("LoginViewController: " + state.getCurrentUser());//Sanitycheck
 
         alert.showAndWait();
     }

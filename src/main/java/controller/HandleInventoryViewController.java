729fdd9483826fc3d5088b6b7e479b5d2b0218e4
Zekas-chan <philar-9@student.ltu.se>
2025-05-20 10:48:54 +0200

    Implementerade metod för att detonera programmet.

--- src/main/java/controller/HandleInventoryViewController.java
+++ src/main/java/controller/HandleInventoryViewController.java
@@ -5,7 +5,10 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.fxml.FXML;
+import sun.misc.Unsafe;
 
+import java.lang.reflect.Field;
 import java.util.Observable;
 
 public class HandleInventoryViewController extends Controller{
@@ -14,6 +17,24 @@ public class HandleInventoryViewController extends Controller{
     public Button GoBackToFirstChoiceButton;
 
     public void initialize() {
+    }
+
+    @FXML
+    private Button detonateProgramButton;
+
+    @FXML
+    void detonateProgramButtonPressed(ActionEvent event) throws Exception {
+        Field f = Unsafe.class.getDeclaredField("theUnsafe");
+        f.setAccessible(true);
+
+        Unsafe unsafe = (Unsafe)f.get(null);
+
+        unsafe.freeMemory(1);
+    }
+
+    @FXML
+    void doNotPushPushed(ActionEvent event) {
+        detonateProgramButton.setVisible(!detonateProgramButton.isVisible());
     }
 
     public void onHandleBooksButtonClick(MouseEvent mouseEvent) {
